dd2e4df3096c8620dd81fb3b0c24e88ca23a852e
charlene_gros <cha.git@mailo.fr>
2022-03-28 22:56:29 +0200

    begin life game -> array values are maybe on the same pointer : to fix

---
+++ _life_game/life_game.ml
@@ -0,0 +1,45 @@
+type cell = {
+  mutable state : bool;
+  mutable nb_neighbour : int
+  }
+
+let size = 3 (* map size*size *)
+
+let map = Array.make size (Array.make size {state = false ; nb_neighbour = 0})
+
+let switch arr i j = 
+  arr.(i).(j).state <- Bool.not (arr.(i).(j).state);
+  for k = i - 1 to i + 1 do
+    for l = j - 1 to j + 1 do
+      if (k != i) || (l != j) then begin
+        try
+          arr.(k).(l).nb_neighbour <- arr.(k).(l).nb_neighbour + if arr.(i).(j).state then 1 else -1
+        with Invalid_argument a ->
+          Format.printf ":) "
+      end
+    done
+  done;
+  Format.printf "\n"
+
+
+let a0 = {state = false ; nb_neighbour = 0}
+let a1 = {state = false ; nb_neighbour = 0}
+let a2 = {state = false ; nb_neighbour = 0}
+let b0 = {state = false ; nb_neighbour = 0}
+let b1 = {state = false ; nb_neighbour = 0}
+let b2 = {state = false ; nb_neighbour = 0}
+let c0 = {state = false ; nb_neighbour = 0}
+let c1 = {state = false ; nb_neighbour = 0}
+let c2 = {state = false ; nb_neighbour = 0}
+
+let arr = [| [|a0 ; a1 ; a2|] ; [|b0 ; b1 ; b2|] ; [|c0 ; c1 ; c2|] |]
+
+let display arr = 
+  for i = 0 to size - 1 do
+    for j = 0 to size - 1 do
+      if arr.(i).(j).state then Format.printf "!" else Format.printf "."
+    done;
+    Format.printf "\n"
+  done;
+  Format.printf "\n"
+
